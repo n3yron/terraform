007a3658d3f4325c1e929961105540e28f41f64c
Александр Довнич <35175316+n3yron@users.noreply.github.com>
2021-02-04 14:39:05 +0200

    Update main.tf

--- vsphere_VM/main.tf
+++ vsphere_VM/main.tf
@@ -1,3 +1,9 @@
+#########################################
+#  ESXI Provider host/login details
+#########################################
+#
+#   Use of variables here to hide/move the variables to a separate file
+#
 provider "esxi" {
 #  version       = ">= 1.3"
   esxi_hostname = var.esxi_hostname
@@ -6,10 +12,29 @@ provider "esxi" {
   esxi_username = var.esxi_username
   esxi_password = var.esxi_password
 }
-resource "esxi_guest" "coreos" {
-  guest_name = "${terraform.workspace}-n3yron"
+
+resource "esxi_guest" "centos" {
+  count = 2
+  guest_name = "k8s-worker-${count.index}"
   disk_store = var.disk_store
-  ovf_source = "https://blog.n3yron.ru/nginx.ova"
+#  guest_startup_timeout = 180
+
+  ovf_source = "/opt/nginx.ova"
+  power      = "on"
+  memsize    = "4096"
+  numvcpus   = "4"
+  network_interfaces {
+    virtual_network = "VM Network" # Required for each network interface, Specify the Virtual Network name.
+  }
+}
+
+resource "esxi_guest" "centos_controller" {
+  count = 1
+  guest_name = "k8s-controller-${count.index}"
+  disk_store = var.disk_store
+#  guest_startup_timeout = 180
+
+  ovf_source = "/opt/nginx.ova"
   power      = "on"
   memsize    = "2048"
   numvcpus   = "2"
